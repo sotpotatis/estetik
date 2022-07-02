912e42475ccbbac8dc4a9b57076cbb4142dd85d3
sotpotatis <albin@albins.website>
2022-07-02 18:22:10 +0200

    Update some scripts

--- automation/discord_bot/run_discord_bot.sh
+++ automation/discord_bot/run_discord_bot.sh
@@ -6,5 +6,5 @@ export ESTETIK_BOT_TOKEN=""
 export ESTETIK_TOKEN=""
 export ESTETIK_API_DOMAIN=""
 export ESTETIK_SSL="True"
-cd /srv/estetik/automation
+cd /srv/estetik/
 python3 automation/discord_bot/main.py #Run the bot
