e83783cb3b9eca3904df268a7cd02a3368f7ef92
sotpotatis <albin@albins.website>
2022-07-03 17:25:53 +0200

    Add missing file

---
+++ app/src/lib/APIRoutes.js
@@ -0,0 +1,3 @@
+export const API_BASE_URL = "http://141.148.244.75:4000/"
+export const API_MEDIA_LIST_URL =  API_BASE_URL + "api/available_media_types/"
+export const API_STRUCTURE_BASE_URL = API_BASE_URL +  "api/structure/"
